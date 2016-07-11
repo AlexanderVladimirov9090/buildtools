8f497279462d39d139d507d7cf5f7a26836c18ee
clouway <alexandervladimirov1902@gmail.com>
2016-07-11 14:05:43 +0300

    Done with Ant Build Tool.

---
+++ src/com/clouway/antdemo/Main.java
@@ -0,0 +1,14 @@
+package com.clouway.antdemo;
+
+/**
+ * Created by clouway on 11.07.16.
+ *
+ * @author Alexander Vladimirov
+ *         (alexandervladimirov1902@gmail.com)
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Ant.");
+  }
+}
